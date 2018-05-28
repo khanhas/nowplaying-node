4a08e9d5ea472319beacd9225c487d47f9faaf20
Nguyen Xuan Khanh <xuankhanh963@gmail.com>
2018-05-28 11:49:06 +0700

    Lint

--- src/NowPlaying-node.cpp
+++ src/NowPlaying-node.cpp
@@ -1,8 +1,8 @@
-#include <node.h>
 #include "NowPlaying.h"
+#include <node.h>
 
 void InitAll(v8::Local<v8::Object> exports, v8::Local<v8::Object> module) {
-	NowPlaying::Init(exports);
+  NowPlaying::Init(exports);
 };
 
 NODE_MODULE(NODE_GYP_MODULE_NAME, InitAll)
